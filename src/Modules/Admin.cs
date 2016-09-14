3653e6b69b8b59cee6dbc89ef30a372436ee1b0d
james7132 <contact@jamessliu.com>
2016-09-14 09:47:09 +0000

    Consolidate Command

--- src/Modules/Admin.cs
+++ src/Modules/Admin.cs
@@ -8,174 +8,173 @@ using Discord.Commands;
 
 namespace DrumBot {
 
-  [Module]
-  [PublicOnly]
-  [ModuleCheck]
-  public class Admin {
+[Module]
+[PublicOnly]
+public class Admin {
 
-    [Command("kick")]
-    [Permission(GuildPermission.KickMembers)]
-    [Remarks("Kicks all mentioned users. Requires ``Kick Members`` permission.")]
-    public async Task Kick(IUserMessage msg, 
-                           params IGuildUser[] users) {
-      var action = await CommandUtility.Action(msg, "kick", async u => await u.KickAsync());
-      await CommandUtility.ForEvery(msg, users, action);
-    }
+  [Command("kick")]
+  [Permission(GuildPermission.KickMembers)]
+  [Remarks("Kicks all mentioned users. Requires ``Kick Members`` permission.")]
+  public async Task Kick(IUserMessage msg, 
+                         params IGuildUser[] users) {
+    var action = await CommandUtility.Action(msg, "kick", async u => await u.KickAsync());
+    await CommandUtility.ForEvery(msg, users, action);
+  }
 
-    [Command("ban")]
-    [Permission(GuildPermission.BanMembers)]
-    [Remarks("Bans all mentioned users. Requires ``Ban Members`` permission.")]
-    public async Task Ban(IUserMessage msg, 
-                          params IGuildUser[] users) {
-      var action = await CommandUtility.Action(msg, "ban", async u => await u.BanAsync());
-      await CommandUtility.ForEvery(msg, users, action);
-    }
+  [Command("ban")]
+  [Permission(GuildPermission.BanMembers)]
+  [Remarks("Bans all mentioned users. Requires ``Ban Members`` permission.")]
+  public async Task Ban(IUserMessage msg, 
+                        params IGuildUser[] users) {
+    var action = await CommandUtility.Action(msg, "ban", async u => await u.BanAsync());
+    await CommandUtility.ForEvery(msg, users, action);
+  }
 
-    [Command("mute")]
-    [Permission(GuildPermission.MuteMembers)]
-    [Remarks("Server mutes all mentioned users. Requires ``Mute Members`` permission.")]
-    public async Task Mute(IUserMessage msg, params IGuildUser[] users) {
-      var action = await CommandUtility.Action(msg, "mute", async u => await u.MuteAsync());
-      await CommandUtility.ForEvery(msg, users, action);
-    }
+  [Command("mute")]
+  [Permission(GuildPermission.MuteMembers)]
+  [Remarks("Server mutes all mentioned users. Requires ``Mute Members`` permission.")]
+  public async Task Mute(IUserMessage msg, params IGuildUser[] users) {
+    var action = await CommandUtility.Action(msg, "mute", async u => await u.MuteAsync());
+    await CommandUtility.ForEvery(msg, users, action);
+  }
 
-    [Command("unmute")]
-    [Permission(GuildPermission.MuteMembers)]
-    [Remarks( "Server unmutes all mentioned users. Requires ``Mute Members`` permission.")]
-    public async Task Unmute(IUserMessage msg, params IGuildUser[] users) {
-      var action = await CommandUtility.Action(msg, "unmute", async u => await u.UnmuteAsync());
-      await CommandUtility.ForEvery(msg, users, action);
-    }
+  [Command("unmute")]
+  [Permission(GuildPermission.MuteMembers)]
+  [Remarks( "Server unmutes all mentioned users. Requires ``Mute Members`` permission.")]
+  public async Task Unmute(IUserMessage msg, params IGuildUser[] users) {
+    var action = await CommandUtility.Action(msg, "unmute", async u => await u.UnmuteAsync());
+    await CommandUtility.ForEvery(msg, users, action);
+  }
 
-    [Command("deafen")]
-    [Permission(GuildPermission.DeafenMembers)]
-    [Remarks( "Server deafens all mentioned users. Requires ``Deafen Members`` permission.")]
-    public async Task Deafen(IUserMessage msg, params IGuildUser[] users) {
-      var action = await CommandUtility.Action(msg, "deafen", async u => await u.DeafenAsync());
-      await CommandUtility.ForEvery(msg, users, action);
-    }
+  [Command("deafen")]
+  [Permission(GuildPermission.DeafenMembers)]
+  [Remarks( "Server deafens all mentioned users. Requires ``Deafen Members`` permission.")]
+  public async Task Deafen(IUserMessage msg, params IGuildUser[] users) {
+    var action = await CommandUtility.Action(msg, "deafen", async u => await u.DeafenAsync());
+    await CommandUtility.ForEvery(msg, users, action);
+  }
 
-    [Command("undeafen")]
-    [Permission(GuildPermission.DeafenMembers)]
-    [Remarks( "Server undeafens all mentioned users. Requires ``Deafen Members`` permission.")]
-    public async Task Undeafen(IUserMessage msg, params IGuildUser[] users) {
-      var action = await CommandUtility.Action(msg, "undeafen", async u => await u.UndeafenAsync());
-      await CommandUtility.ForEvery(msg, users, action);
-    }
+  [Command("undeafen")]
+  [Permission(GuildPermission.DeafenMembers)]
+  [Remarks( "Server undeafens all mentioned users. Requires ``Deafen Members`` permission.")]
+  public async Task Undeafen(IUserMessage msg, params IGuildUser[] users) {
+    var action = await CommandUtility.Action(msg, "undeafen", async u => await u.UndeafenAsync());
+    await CommandUtility.ForEvery(msg, users, action);
+  }
 
-    [Command("nickname")]
-    [Remarks("Sets the nickname of all mentioned users, or nicknames yourself.\nIf no ``users`` is empty, nicknames the user who used the command"
-    + "and requires the ``Change Nickname`` permission.\nIf at least one ``user`` is specified, nicknames the mentioned users and requires the "
-    + "``Manage Nicknames`` permission.")]
-    public async Task Nickname(IUserMessage msg, string nickname, params IGuildUser[] users) {
-      Check.InGuild(msg);
-      var author = msg.Author as IGuildUser;
-      IGuildUser[] allUsers = users;
-      if (allUsers.Length <= 0) {
-        if(!author.GuildPermissions.ChangeNickname) {
-          await msg.Respond($"{author.Mention} you do not have the ``Change Nickname`` permission. See ``{Config.CommandPrefix}help nickname``");
-          return;
-        }
-        allUsers = new[] {msg.Author as IGuildUser};
-      }
-      if(!author.GuildPermissions.ManageNicknames) {
-        await msg.Respond($"{author.Mention} you do not have the ``Manage Nicknames`` permission. See ``{Config.CommandPrefix}help nickname``");
+  [Command("nickname")]
+  [Remarks("Sets the nickname of all mentioned users, or nicknames yourself.\nIf no ``users`` is empty, nicknames the user who used the command"
+  + "and requires the ``Change Nickname`` permission.\nIf at least one ``user`` is specified, nicknames the mentioned users and requires the "
+  + "``Manage Nicknames`` permission.")]
+  public async Task Nickname(IUserMessage msg, string nickname, params IGuildUser[] users) {
+    Check.InGuild(msg);
+    var author = msg.Author as IGuildUser;
+    IGuildUser[] allUsers = users;
+    if (allUsers.Length <= 0) {
+      if(!author.GuildPermissions.ChangeNickname) {
+        await msg.Respond($"{author.Mention} you do not have the ``Change Nickname`` permission. See ``{Config.CommandPrefix}help nickname``");
         return;
       }
-
-      var action = await CommandUtility.Action(msg, "nickname", async u => await u.SetNickname(nickname));
-      await CommandUtility.ForEvery(msg, allUsers, action);
+      allUsers = new[] {msg.Author as IGuildUser};
+    }
+    if(!author.GuildPermissions.ManageNicknames) {
+      await msg.Respond($"{author.Mention} you do not have the ``Manage Nicknames`` permission. See ``{Config.CommandPrefix}help nickname``");
+      return;
     }
 
-    [Group("prune")]
-    public class PruneGroup {
+    var action = await CommandUtility.Action(msg, "nickname", async u => await u.SetNickname(nickname));
+    await CommandUtility.ForEvery(msg, allUsers, action);
+  }
 
-      [Command]
-      [Permission(GuildPermission.ManageMessages)]
-      [Remarks("Removes the last X messages from the current channel. Requires ``Manage Messages`` permission.")]
-      public Task Prune(IUserMessage msg, int count = 100) {
-        return PruneMessages(Check.InGuild(msg), m => true, count);
-      }
+  [Group("prune")]
+  public class PruneGroup {
 
-      [Command("user")]
-      [Permission(GuildPermission.ManageMessages)]
-      [Remarks("Removes all messages from all mentioned users in the last 100 messages. Requires ``Manage Messages`` permission.")]
-      public Task PruneUser(IUserMessage msg, params IGuildUser[] users) {
-        var userSet = new HashSet<IUser>(users);
-        return PruneMessages(Check.InGuild(msg), m => userSet.Contains(m.Author));
-      }
-
-      [Command("embed")]
-      [Permission(GuildPermission.ManageMessages)]
-      [Remarks("Removes all messages with embeds or attachments in the last X messages. Requires ``Manage Messages`` permission.")]
-      public Task PruneEmbed(IUserMessage msg, int count = 100) {
-        return PruneMessages(Check.InGuild(msg), m => m.Embeds.Any() || m.Attachments.Any(), count);
-      }
-
-      [Command("mine")]
-      [Remarks("Removes all messages from the user using the command in the last X messages.")]
-      public Task PruneMine(IUserMessage msg, int count = 100) {
-        ulong id = msg.Author.Id;
-        return PruneMessages(Check.InGuild(msg), m => m.Author.Id == id, count);
-      }
-
-      [Command("bot")]
-      [Permission(GuildPermission.ManageMessages)]
-      [Remarks("Removes all messages from all bots in the last X messages. Requires ``Manage Messages`` permission.")]
-      public Task PruneBot(IUserMessage msg, int count = 100) {
-        return PruneMessages(Check.InGuild(msg), m => m.Author.IsBot, count);
-      }
-
-      static async Task PruneMessages(IMessageChannel channel,
-                                    Func<IMessage, bool> pred = null,
-                                    int count = 100) {
-        if (count > Config.PruneLimit)
-          count = Config.PruneLimit;
-        if (count < 0) {
-          await channel.Respond("Cannot prune a negative count of messages");
-          return;
-        }
-        var finalCount = count;
-        var messages = await channel.GetMessagesAsync(count);
-        IEnumerable<IMessage> allMessages = messages;
-        if (pred != null) {
-          var filtered = messages.Where(pred).ToArray();
-          finalCount = Math.Min(finalCount, filtered.Length);
-          allMessages = filtered;
-        }
-        await channel.DeleteMessagesAsync(allMessages
-                        .OrderByDescending(m => m.Timestamp)
-                        .Take(count));
-        await channel.Success($"Deleted {finalCount} messages.");
-      }
+    [Command]
+    [Permission(GuildPermission.ManageMessages)]
+    [Remarks("Removes the last X messages from the current channel. Requires ``Manage Messages`` permission.")]
+    public Task Prune(IUserMessage msg, int count = 100) {
+      return PruneMessages(Check.InGuild(msg), m => true, count);
     }
 
-    [Group("channel")]
-    public class ChannelGroup {
-
-      [Command("create")]
-      [Permission(GuildPermission.ManageChannels)]
-      [Remarks("Creates a public channel with a specified name. Requires ``Manage Channels`` permission.")]
-      public async Task ChannelCreate(IUserMessage msg, string name) {
-        var guild = Check.InGuild(msg).Guild;
-        var channel = await guild.CreateTextChannelAsync(name); 
-        await msg.Success($"{channel.Mention} created.");
-      }
-
-      [Command("delete")]
-      [Permission(GuildPermission.ManageChannels)]
-      [Remarks("Deletes all mentioned channels. Requires ``Manage Channels`` permission.")]
-      public Task ChannelDelete(IUserMessage msg, params IGuildChannel[] channels) {
-        return CommandUtility.ForEvery(msg, channels, CommandUtility.Action(
-          async delegate(IGuildChannel channel) {
-            await channel.DeleteAsync();
-          }));
-      }
-
+    [Command("user")]
+    [Permission(GuildPermission.ManageMessages)]
+    [Remarks("Removes all messages from all mentioned users in the last 100 messages. Requires ``Manage Messages`` permission.")]
+    public Task PruneUser(IUserMessage msg, params IGuildUser[] users) {
+      var userSet = new HashSet<IUser>(users);
+      return PruneMessages(Check.InGuild(msg), m => userSet.Contains(m.Author));
     }
 
-    [Group("role")]
-    public class RoleGroup {
+    [Command("embed")]
+    [Permission(GuildPermission.ManageMessages)]
+    [Remarks("Removes all messages with embeds or attachments in the last X messages. Requires ``Manage Messages`` permission.")]
+    public Task PruneEmbed(IUserMessage msg, int count = 100) {
+      return PruneMessages(Check.InGuild(msg), m => m.Embeds.Any() || m.Attachments.Any(), count);
+    }
+
+    [Command("mine")]
+    [Remarks("Removes all messages from the user using the command in the last X messages.")]
+    public Task PruneMine(IUserMessage msg, int count = 100) {
+      ulong id = msg.Author.Id;
+      return PruneMessages(Check.InGuild(msg), m => m.Author.Id == id, count);
+    }
+
+    [Command("bot")]
+    [Permission(GuildPermission.ManageMessages)]
+    [Remarks("Removes all messages from all bots in the last X messages. Requires ``Manage Messages`` permission.")]
+    public Task PruneBot(IUserMessage msg, int count = 100) {
+      return PruneMessages(Check.InGuild(msg), m => m.Author.IsBot, count);
+    }
+
+    static async Task PruneMessages(IMessageChannel channel,
+                                  Func<IMessage, bool> pred = null,
+                                  int count = 100) {
+      if (count > Config.PruneLimit)
+        count = Config.PruneLimit;
+      if (count < 0) {
+        await channel.Respond("Cannot prune a negative count of messages");
+        return;
+      }
+      var finalCount = count;
+      var messages = await channel.GetMessagesAsync(count);
+      IEnumerable<IMessage> allMessages = messages;
+      if (pred != null) {
+        var filtered = messages.Where(pred).ToArray();
+        finalCount = Math.Min(finalCount, filtered.Length);
+        allMessages = filtered;
+      }
+      await channel.DeleteMessagesAsync(allMessages
+                      .OrderByDescending(m => m.Timestamp)
+                      .Take(count));
+      await channel.Success($"Deleted {finalCount} messages.");
+    }
+  }
+
+  [Group("channel")]
+  public class ChannelGroup {
+
+    [Command("create")]
+    [Permission(GuildPermission.ManageChannels)]
+    [Remarks("Creates a public channel with a specified name. Requires ``Manage Channels`` permission.")]
+    public async Task ChannelCreate(IUserMessage msg, string name) {
+      var guild = Check.InGuild(msg).Guild;
+      var channel = await guild.CreateTextChannelAsync(name); 
+      await msg.Success($"{channel.Mention} created.");
+    }
+
+    [Command("delete")]
+    [Permission(GuildPermission.ManageChannels)]
+    [Remarks("Deletes all mentioned channels. Requires ``Manage Channels`` permission.")]
+    public Task ChannelDelete(IUserMessage msg, params IGuildChannel[] channels) {
+      return CommandUtility.ForEvery(msg, channels, CommandUtility.Action(
+        async delegate(IGuildChannel channel) {
+          await channel.DeleteAsync();
+        }));
+    }
+
+  }
+
+  [Group("role")]
+  public class RoleGroup {
     const string Requirement = " Requires ``Manage Role`` permission for both user and bot.";
 
     [Command("add")]
@@ -241,8 +240,8 @@ namespace DrumBot {
     [Command("delete")]
     [Permission(GuildPermission.ManageRoles)]
     [Remarks("Deletes a role and removes it from all users.")]
-    public async Task RoleDelete(IUserMessage msg, params IRole[] roles) {
-      await CommandUtility.ForEvery(msg, roles, CommandUtility.Action(
+    public Task RoleDelete(IUserMessage msg, params IRole[] roles) {
+      return CommandUtility.ForEvery(msg, roles, CommandUtility.Action(
         async delegate(IRole role) {
           await role.DeleteAsync(); 
         }));
@@ -285,18 +284,18 @@ namespace DrumBot {
         return true;
       return false;
     }
-  }
+}
 
-  static async Task RoleCommand(IUserMessage m, IRole role, string action, IEnumerable<IGuildUser> users, Func<IGuildUser, IRole, Task> task) {
-    var guild = Check.InGuild(m).Guild;
-    var selfUser = await Bot.Client.GetCurrentUserAsync();
-    var guildBot = await guild.GetUserAsync(selfUser.Id);
-    if (!Utility.RoleCheck(guildBot, role))
-      throw new RoleRankException($"{guildBot.Username} cannot {action} role \"{role.Name}\", as it is above my roles.");
-    if (!Utility.RoleCheck(m.Author as IGuildUser, role))
-      throw new RoleRankException($"{m.Author.Username}, you cannot {action} role \"{role.Name}\", as it is above their roles.");
-    await CommandUtility.ForEvery(m, users,
-      await CommandUtility.Action(m, action + " role", user => task(user, role)));
-    }
+static async Task RoleCommand(IUserMessage m, IRole role, string action, IEnumerable<IGuildUser> users, Func<IGuildUser, IRole, Task> task) {
+  var guild = Check.InGuild(m).Guild;
+  var selfUser = await Bot.Client.GetCurrentUserAsync();
+  var guildBot = await guild.GetUserAsync(selfUser.Id);
+  if (!Utility.RoleCheck(guildBot, role))
+    throw new RoleRankException($"{guildBot.Username} cannot {action} role \"{role.Name}\", as it is above my roles.");
+  if (!Utility.RoleCheck(m.Author as IGuildUser, role))
+    throw new RoleRankException($"{m.Author.Username}, you cannot {action} role \"{role.Name}\", as it is above their roles.");
+  await CommandUtility.ForEvery(m, users,
+    await CommandUtility.Action(m, action + " role", user => task(user, role)));
   }
+}
 }
