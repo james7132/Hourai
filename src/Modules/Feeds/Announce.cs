76ee42e8a9f4288c88c92f4d1d6e378fc3834e73
james7132 <james.siqi.liu@gmail.com>
2016-11-03 07:33:49 +0000

    Add command documentation for the announce commands

--- src/Modules/Feeds/Announce.cs
+++ src/Modules/Feeds/Announce.cs
@@ -14,18 +14,22 @@ public partial class Feeds {
     }
 
     [Command("join")]
+    [Remarks("Enables or disables server join messages in the current channel")]
     [Permission(GuildPermission.ManageGuild, Require.BotOwnerOverride)]
     public Task Join() => SetMessage(c => c.JoinMessage = !c.JoinMessage, "Join");
 
     [Command("leave")]
+    [Remarks("Enables or disables server leave messages in the current channel")]
     [Permission(GuildPermission.ManageGuild, Require.BotOwnerOverride)]
     public Task Leave() => SetMessage(c => c.LeaveMessage = !c.LeaveMessage, "Leave");
 
     [Command("ban")]
+    [Remarks("Enables or disables server ban messages in the current channel")]
     [Permission(GuildPermission.ManageGuild, Require.BotOwnerOverride)]
     public Task Ban() => SetMessage(c => c.BanMessage = !c.BanMessage, "Ban");
 
     [Command("voice")]
+    [Remarks("Enables or disables voice messages in the current channel")]
     [Permission(GuildPermission.ManageGuild, Require.BotOwnerOverride)]
     public Task Voice() => SetMessage(c => c.VoiceMessage = !c.VoiceMessage, "Voice");
 
