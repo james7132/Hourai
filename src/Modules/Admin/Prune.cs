43d512f1b1db6e38737bd324c6a292fcf543b89d
james7132 <contact@jamessliu.com>
2017-02-06 09:54:04 +0000

    Add prune reactions

--- src/Modules/Admin/Prune.cs
+++ src/Modules/Admin/Prune.cs
@@ -3,10 +3,37 @@ using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Hourai.Preconditions;
 
 namespace Hourai.Modules {
+
+public static class AsyncEnumerable {
+
+  public static Task ForEachAwait<T>(
+      this IAsyncEnumerable<T> enumerable,
+      Func<T, Task> action) {
+    return ForEachAwait(enumerable, action, CancellationToken.None);
+  }
+
+  public static async Task ForEachAwait<T>(
+      this IAsyncEnumerable<T> enumerable,
+      Func<T, Task> action,
+      CancellationToken cancellationToken) {
+    using (var enumerator = enumerable.GetEnumerator()) {
+      if (await enumerator.MoveNext(cancellationToken).ConfigureAwait(continueOnCapturedContext: false)) {
+        Task<bool> moveNextTask;
+        do {
+          var current = enumerator.Current;
+          moveNextTask = enumerator.MoveNext(cancellationToken);
+          await action(current); //now with await
+        } while (await moveNextTask.ConfigureAwait(continueOnCapturedContext: false));
+      }
+    }
+  }
+
+}
 
 public partial class Admin {
 
@@ -56,6 +83,26 @@ public partial class Admin {
     [Remarks("Removes all messages from all bots in the last X messages.")]
     public Task Bot(int count = 100) =>
       PruneMessages(m => m.Author.IsBot, count);
+
+    [Command("reaction")]
+    [ChannelRateLimit(1, 15)]
+    [RequirePermission(GuildPermission.AddReactions)]
+    [RequirePermission(GuildPermission.ManageMessages)]
+    [Remarks("Removes all reactions from messages in the last X messages.")]
+    public async Task Reactions(int count = 100) {
+      var state = Context.Channel.EnterTypingState();
+      await Context.Channel.GetMessagesAsync(count).ForEachAwait(async m => {
+            IEnumerable<IUserMessage> messages = m.OfType<IUserMessage>()
+              .Where(message => message.Reactions.Count > 0);
+            foreach (IUserMessage message in messages) {
+              Log.Info(message.Author.ToString());
+              await message.RemoveAllReactionsAsync();
+              await Task.Delay(2000);
+            }
+        });
+      state.Dispose();
+      await Success();
+    }
 
     async Task PruneMessages(Func<IMessage, bool> pred = null,
         int count = 100) {
