f43e64c2d82dd22bb53d99e2bf9398c419d2ae13
james7132 <contact@jamessliu.com>
2017-02-11 01:59:16 +0000

    Complete ErrorService

---
+++ src/Services/ErrorService.cs
@@ -0,0 +1,58 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Hourai {
+
+  public class ErrorService  {
+
+    DiscordShardedClient Client { get; }
+    Bot Bot { get; }
+    IMessageChannel OwnerChannel { get; set; }
+    List<Exception> Exceptions { get; }
+
+    public ErrorService(IDependencyMap map) {
+      Client = map.Get<DiscordShardedClient>();
+      Bot = map.Get<Bot>();
+      Bot.RegularTasks += SendErrors;
+      Exceptions = new List<Exception>();
+    }
+
+    async Task SendErrors() {
+      if (Exceptions.Count <= 0)
+        return;
+      if (OwnerChannel == null)
+        OwnerChannel = await Bot.Owner.CreateDMChannelAsync();
+      foreach(var exception in Exceptions.ToArray()) {
+        try {
+          await SendError(exception);
+          Exceptions.Remove(exception);
+          await Task.Delay(1000);
+        } catch(Exception e) {
+          Log.Error(e);
+          Exceptions.Add(e);
+        }
+      }
+    }
+
+    public async void RegisterException(Exception e) {
+      if (OwnerChannel == null)
+        OwnerChannel = await Bot.Owner.CreateDMChannelAsync();
+      try {
+        await SendError(e);
+      } catch {
+        Exceptions.Add(e);
+      }
+    }
+
+    Task SendError(Exception exception) {
+      return OwnerChannel.SendMessageAsync(exception.ToString().Ellipisize(400).Code());
+    }
+
+  }
+
+
+}
