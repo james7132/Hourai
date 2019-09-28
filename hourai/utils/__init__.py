4c72639139c7e0e4e90d9508985f0bc7d9427675
james7132 <james.siqi.liu@gmail.com>
2019-09-28 12:04:40 +0000

    Make Nitro functions utility functions

--- hourai/utils/__init__.py
+++ hourai/utils/__init__.py
@@ -10,6 +10,7 @@ from hourai import config
 MODERATOR_PREFIX = 'mod'
 DELETED_USER_REGEX = re.compile('Deleted User\s+[0-9a-fA-F]+')
 
+
 async def broadcast(channels, *args, **kwargs):
     """
     Broadcasts a message to multiple channels at once.
@@ -17,6 +18,7 @@ async def broadcast(channels, *args, **kwargs):
     """
     tasks = [ch.send(*args, **kwargs) for ch in channels if ch is not None]
     return await asyncio.gather(*tasks)
+
 
 async def success(ctx, suffix=None):
     success = config.get_config_value(config.get_config(), 'success_response',
@@ -26,6 +28,7 @@ async def success(ctx, suffix=None):
     else:
         await ctx.send(config.SUCCESS_RESPONSE)
 
+
 def pretty_print(resource):
     output = []
     if hasattr(resource, 'name'):
@@ -34,17 +37,20 @@ def pretty_print(resource):
         output.append('({})'.format(resource.id))
     return ' '.join(output)
 
+
 async def maybe_coroutine(f, *args, **kwargs):
     value = f(*args, **kwargs)
     if inspect.isawaitable(value):
         return await value
     return value
 
+
 async def collect(async_iter):
     vals = []
     async for val in async_iter:
         vals.append(val)
     return vals
+
 
 def log_time(func):
     """ Logs the time to run a function to std out. """
@@ -75,13 +81,16 @@ def log_time(func):
                   func.__qualname__, real_time, cpu_time))
     return async_time_logger if inspect.iscoroutinefunction(func) else time_logger
 
+
 async def send_dm(user, *args, **kwargs):
     """ Shorthand to send a user a DM. """
     dm_channel = user.dm_channel or await user.create_dm()
     await dm_channel.send(*args, **kwargs)
 
+
 def any_in(population, seq):
     return any(val in population for val in seq)
+
 
 def is_deleted_user(user):
     """ Checks if a user is deleted or not by Discord. Works on discord.User
@@ -89,42 +98,52 @@ def is_deleted_user(user):
     """
     return user.avatar is None and DELETED_USER_REGEX.match(user.name)
 
+
 def is_moderator(member):
     """ Checks if a user is a moderator. """
     return any(is_moderator_role(r) for r in member.roles)
+
 
 def is_moderator_role(role):
     """ Checks if a role is a moderator role. """
     return role.permissions.administrator or role.name.lower().startswith(MODERATOR_PREFIX)
 
+
 def is_online(member):
     return member.status == discord.Status.online
+
 
 def all_with_roles(members, roles):
     """ Filters a list of members to those with roles. Returns a generator of members """
     role_set = set(roles)
     return filter(lambda m: any_in(role_set, m.roles), members)
 
+
 def all_without_roles(members, roles):
     """ Filters a list of members to those without roles. Returns a generator of members """
     role_set = set(roles)
     return filter(lambda m: not any_in(role_set, m.roles), members)
 
+
 def find_moderator_roles(guild):
     """ Finds all of the moderator roles on a server. Returns an generator of roles """
     return filter(lambda r: is_moderator_role(r), guild.roles)
+
 
 def find_moderators(guild):
     """ Finds all of the moderators on a server. Returns a generator of members. """
     return all_with_roles(guild.members, find_moderator_roles(guild))
 
+
 def find_bots(guild):
     """ Finds all of the bots on a server. Returns a generator of members. """
     return filter(lambda m: m.bot, guild.members)
 
+
 def find_online_moderators(guild):
     """ Finds all of the online moderators on a server. Returns a generator of members. """
     return filter(is_online, find_moderators(guild))
+
 
 def mention_random_online_mod(guild):
     """
@@ -136,3 +155,16 @@ def mention_random_online_mod(guild):
         return random.choice(moderators).mention
     else:
         return f'{ctx.guild.owner.mention}, no mods are online!'
+
+
+def is_nitro_booster(bot, member):
+    """Checks if the user is boosting any server the bot is on."""
+    return any(m.id == member.id
+               for g in bot.guilds
+               for m in g.premium_subscribers)
+
+
+def has_nitro(bot, member):
+    """Checks if the user has Nitro, may have false negatives."""
+    return member.is_avatar_animated() or \
+           __is_nitro_booster(bot, member)
