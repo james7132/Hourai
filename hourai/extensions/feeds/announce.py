2d3decac8342d29d1963d18096afb070056dff0c
james7132 <james.siqi.liu@gmail.com>
2019-10-01 23:56:20 +0000

    Join/Leave/Ban announcements

---
+++ hourai/extensions/feeds/announce.py
@@ -0,0 +1,62 @@
+import asyncio
+import discord
+import random
+from hourai import cogs
+from hourai.db import proto
+from discord.ext import commands
+
+class Announce(cogs.BaseCog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def get_announce_config(self, guild):
+        config = await self.bot.storage.announce_configs.get(guild.id)
+        return config or proto.AnnoucementConfig()
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        announce_config = self.get_announce_config(member.guild)
+        if not announce_config.HasField('joins'):
+            return
+        if len(announce_config.joins.messages) > 0:
+            choices = list(announce_config.joins.messages)
+        else:
+            choices = [f'**{member.mention}** has joined the server.']
+        await self.__make_announcement(member.guild, announce_config.joins,
+                                       choices)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        announce_config = self.get_announce_config(member.guild)
+        if not announce_config.HasField('leaves'):
+            return
+        if len(announce_config.leaves.messages) > 0:
+            choices = list(announce_config.leaves.messages)
+        else:
+            choices = [f'**{member.mention}** has left the server.']
+        await self.__make_announcement(member.guild, announce_config.leaves,
+                                       choices)
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, member):
+        announce_config = self.get_announce_config(member.guild)
+        if not announce_config.HasField('bans'):
+            return
+        if len(announce_config.bans.messages) > 0:
+            choices = list(announce_config.bans.messages)
+        else:
+            choices = [f'**{member.mention** has been banned.']
+        await self.__make_announcement(member.guild, announce_config.leaves,
+                                       choices)
+
+    async def __make_announcement(self, guild, config, choices):
+        assert len(choices) > 0
+        channels = [guild.get_channel(ch_id) for ch_id in config.channel_ids]
+        channels = [ch for ch in channels
+                    if isinstance(ch, discord.TextChannel)]
+        tasks = []
+        for channel in channels:
+            content = random.choice(config.messages)
+            tasks.append(channel.send(content))
+        await asyncio.gather(*tasks)
