c683e76bb877a04aacad6d9d5078da1ea05bd151
james7132 <james.siqi.liu@gmail.com>
2019-10-07 17:40:11 +0000

    FIX - Fix removal of a single item from the music queues

--- hourai/extensions/music/queue.py
+++ hourai/extensions/music/queue.py
@@ -5,9 +5,18 @@ from hourai.utils.iterable import first
 
 
 class MusicQueue(asyncio.Queue):
+    """An asyncio.Queue implementation that forms a FIFO, round-robin queue
+    based on the provided key. The input is expected to be a tuple of
+    (key, value). Backed by a collections.OrderedDict of lists, batch operations
+    over individual queues for each key are either O(1) or O(k) operations, where
+    k is the number of entries under that key.
 
-    def __init__(self):
-        super().__init__()
+    Example:
+      Input: (a, 1), (a, 2), (b, 1), (c, 1), (a, 3), (b, 2)
+      Output: (a, 1), (b, 1), (c, 1), (a, 2), (b, 2), (a, 3)
+    """
+
+    def __init(self, maxsize):
         self._queue = collections.OrderedDict()
 
     def _put(self, item):
@@ -26,22 +35,38 @@ class MusicQueue(asyncio.Queue):
         if not found:
             return None
         item = latest_queue.pop(0)
-        return key, item
+        return (key, item)
 
     def shuffle(self, key):
-        """ Shuffles the items for a given key. """
+        """Shuffles the items for a given key. This is an O(k) operation, where
+        k is the number of elements queued for the given key.
+        """
         queue = self._queue.get(key)
         if queue is not None:
             random.shuffle(queue)
-        return len(queue)
+        return len(queue) if queue is not None else 0
 
     def clear(self):
-        """ Clears all elements in the queue. """
+        """Clears all elements in the queue. This is an O(1) operation."""
         self._queue.clear()
         self._wakeup_next(self._putters)
 
+    def remove(self, idx):
+        """Removes an item by it's index in the queue. This is a O(n) operation
+        as it requires iteration through the queue.
+        """
+        if idx < 0 or idx >= len(self):
+            raise IndexError
+        for key, queue_idx, queue in self.__iter_self():
+            idx -= 1
+            if idx == 0:
+                return (key, queue.pop(queue_idx))
+        raise IndexError
+
     def remove_all(self, key):
-        """ Clears all elements in the queue with a given key. """
+        """Clears all elements in the queue with a given key. This is a O(1)
+        operation.
+        """
         if key in self._queue:
             count = len(self._queue[key])
             del self._queue[key]
@@ -49,17 +74,32 @@ class MusicQueue(asyncio.Queue):
             return count
         return 0
 
+    def __getitem__(self, idx):
+        """Gets an item by it's index in the queue. This is a O(n) operation as
+        it requires iteration through the queue.
+        """
+        if idx < 0 or idx >= len(self):
+            raise IndexError
+        for key, queue_idx, queue in self.__iter_self():
+            idx -= 1
+            if idx == 0:
+                return (key, queue[queue_idx])
+        # This shouldn't happen with the initial check
+        raise IndexError
+
     def __iter__(self):
-        iters = [(key, iter(value)) for key, value in self._queue.items()]
-        progressed = True
-        while progressed:
-            progressed = False
-            for key, iterator in iters:
-                try:
-                    yield (key, next(iterator))
-                    progressed = True
-                except StopIteration:
-                    pass
+        for key, queue_idx, queue in self.__iter_self():
+            yield (key, queue[queue_idx])
+
+    def __iter_self(self):
+        indexes = collections.deque((key, 0) for key in self._queue.keys())
+        while len(indexes) > 0:
+            key, queue_idx = indexes.popleft()
+            queue = self._queue[key]
+            if idx >= len(queue):
+                continue
+            yield (key, queue_idx, queue)
+            indexes.append((key, queue_idx + 1))
 
     def __len__(self):
         if len(self._queue) <= 0:
