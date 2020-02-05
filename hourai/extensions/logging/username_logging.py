e42688d257c5efd50dbbec41625411933fb9ebb8
james7132 <james.siqi.liu@gmail.com>
2020-02-05 19:33:07 +0000

    Remove username cleanup loop

--- hourai/extensions/logging/username_logging.py
+++ hourai/extensions/logging/username_logging.py
@@ -19,13 +19,7 @@ class UsernameLogging(BaseCog):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
-        # self.cleanup_username_histories.start()
         self.pending_ids = None
-        self.offset = 0
-
-    def cog_unload(self):
-        # self.cleanup_username_histories.cancel()
-        pass
 
     @commands.Cog.listener()
     async def on_user_update(self, before, after):
@@ -64,43 +58,6 @@ class UsernameLogging(BaseCog):
     async def on_group_remove(self, group, user):
         self.bot.loop.create_task(self.log_username_change(user))
 
-    @tasks.loop(seconds=0.1)
-    async def cleanup_username_histories(self):
-        frame_size = 5000
-        try:
-            with self.bot.create_storage_session() as session:
-                ids = session.query(Username.user_id) \
-                             .group_by(Username.user_id) \
-                             .offset(self.offset).limit(frame_size)
-
-                ids = [x[0] for x in ids]
-
-                if len(ids) <= 0:
-                    self.offset = 0
-                    return
-
-                keys = lambda u: u.user_id
-
-                usernames = session.query(Username) \
-                                   .filter(Username.user_id.in_(ids)) \
-                                   .all()
-                usernames = list(usernames)
-                usernames.sort(key=keys)
-                for user_id, names in itertools.groupby(usernames, key=keys):
-                    self.merge_names(names, session)
-
-                if len(session.deleted) > 0:
-                    self.log_changes(session)
-                    session.commit()
-                else:
-                    self.offset += frame_size
-        except Exception:
-            self.bot.logger.exception('Exception while clearing histories:')
-
-    @cleanup_username_histories.before_loop
-    async def before_cleanup_username_histories(self):
-        await self.bot.wait_until_ready()
-
     @commands.command()
     @commands.is_owner()
     async def refresh(self, ctx):
@@ -116,10 +73,6 @@ class UsernameLogging(BaseCog):
 
         timestamp = datetime.utcnow()
 
-        def create_username():
-            return Username(user_id=user.id, name=user.name,
-                            timestamp=timestamp,
-                            discriminator=user.discriminator)
         logged = False
         backoff = 1
         while not logged:
@@ -131,7 +84,9 @@ class UsernameLogging(BaseCog):
                     usernames = list(usernames)
                     if any(n.name == user.name for n in usernames):
                         return
-                    username = create_username()
+                    username = Username(user_id=user.id, name=user.name,
+                                        timestamp=timestamp,
+                                        discriminator=user.discriminator)
                     usernames.append(username)
                     filtered = self.merge_names(usernames, session)
                     if username in filtered:
