c86fb61b57884658d77ba1fb6291bdfe2cb6628f
james7132 <contact@jamessliu.com>
2020-06-02 04:55:22 +0000

    Add scripts for database backups

---
+++ scripts/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script for backing up the databases for Hourai
+
+tmp_dir=$(mktemp -d -t hourai-backup-XXXXXXXXXX)
+
+now=$(date +%Y-%m-%d"_"%H_%M_%S)
+archive=$(pwd)/dump_$now.tar.gz
+
+psql_dump="$tmp_dir/dump.psql"
+redis_dump="$tmp_dir/dump.rdb"
+
+docker exec postgres-$HOURAI_ENV pg_dumpall -c -U hourai > $psql_dump
+docker cp redis-$HOURAI_ENV:/data/dump.rdb $redis_dump
+
+cd $tmp_dir
+tar -czvf $archive *
+rm -rf $tmp_dir
