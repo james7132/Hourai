4dd324374f6e8aae195caaf141cb553fc0b993ed
james7132 <contact@jamessliu.com>
2020-06-17 04:55:10 -0700

    [Web] Improve visual look by using Discord colors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,9 +9,18 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~bulma/sass/utilities/_all";
 @import "@/scss/_variables.scss";
 @import "~bulma";
-@import "@/scss/_overrides.scss";
 @import "~buefy/src/scss/buefy";
+@import "@/scss/_overrides.scss";
+
+html {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  width: 100%;
+  min-height: 100%;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import '@/vendor.js'
 
-import App from '@/App.vue'
 import router from '@/router.js'
 import {API, store} from '@/store.js'
+import App from '@/App.vue'
 
 Vue.config.productionTip = false
 Vue.prototype.$api = API
