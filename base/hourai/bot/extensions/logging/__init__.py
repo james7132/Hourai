2108a8ac4d40fd19562c1b1e589b4ab1f0ad3a11
james7132 <contact@jamessliu.com>
2020-06-25 04:04:25 -0700

    [Bot] Save member roles for restoring to users

--- base/hourai/bot/extensions/logging/__init__.py
+++ base/hourai/bot/extensions/logging/__init__.py
@@ -1,6 +1,7 @@
 from .mod_logging import ModLogging
 from .username_logging import UsernameLogging
 from .owner_logging import OwnerLogging
+from .role_logging import RoleLogging
 from .counters import Counters
 
 
@@ -8,6 +9,7 @@ def setup(bot):
     cogs = (ModLogging(bot),
             UsernameLogging(bot),
             OwnerLogging(bot),
+            RoleLogging(bot),
             Counters(bot))
     for cog in cogs:
         bot.add_cog(cog)
---
+++ base/hourai/bot/extensions/logging/role_logging.py
@@ -0,0 +1,72 @@
+from discord.ext import commands
+from hourai.bot import cogs
+from hourai.db import models
+from hourai.utils import iterable
+
+
+class RoleLogging(cogs.BaseCog):
+    """ Cog for logging role changes. """
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        # TODO(james7132): Restore saved roles
+        self.log_member_roles(member)
+
+    @commands.Cog.listener()
+    async def on_user_update(self, before, after):
+        self.log_member_roles(member)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        self.log_member_roles(member)
+
+    @commands.Cogs.listener()
+    async def on_guild_join(self, guild):
+        await self.log_guild_roles(guild)
+
+    @commands.Cogs.listener()
+    async def on_guild_remove(self, guild):
+        self.clear_guild(guild)
+
+    async def log_guild_roles(self, guild):
+        model = models.MemberRoles
+        async for chunk in iterable.chunk_async(guild.fetch_members(limit=None),
+                                                chunk_size=1000):
+            roles = {member.id: self.create_member_roles(member)
+                     for member in chunk}
+            with self.bot.create_storage_session() as session:
+                existing = session.query(model) \
+                                  .filter_by(guild_id=guild.id) \
+                                  .filter(model.user_id.in_(roles.keys())) \
+                                  .all()
+
+                for roles in existing:
+                    # Only merge those posts which already exist in the database
+                    session.merge(my_new_posts.pop(roles.member_id))
+
+                # Only add those posts which did not exist in the database
+                session.add_all(roles.values())
+                session.commit()
+
+    def log_member_roles(self, member):
+        member_roles = self.create_member_roles(member)
+        with self.bot.create_storage_session() as session:
+            session.add(member_roles)
+            session.commit()
+
+    def clear_guild(self, guild):
+        with self.bot.create_storage_session() as session:
+            session.query(models.MemberRoles).filter(
+                model.MemberRoles.guild_id==guild.id).delete()
+
+    def create_member_roles(self, member):
+        role_ids = list(member._roles)
+        role_ids.remove(member.guild.default_role.id)
+        return models.MemberRoles(
+            guild_id=member.guild.id,
+            user_id=member.id,
+            role_ids=role_ids)
