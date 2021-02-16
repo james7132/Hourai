fd14c30af1035ee63603fc809bbc84a537843587
james7132 <contact@jamessliu.com>
2021-02-16 14:07:23 -0800

    Remove LRUCache from Cache implementation.
    Not currently needed, just uses up extra memory

--- base/hourai/db/caches.py
+++ base/hourai/db/caches.py
@@ -9,67 +9,6 @@ from coders import IdentityCoder
 from .redis_utils import redis_transaction
 
 
-class LRUCache:
-    """A LRU (Least Recently Used) key-value cache with optional TTL for items
-    in it. Supports storing any value and hashable key. Not threadsafe.
-    """
-
-    __slots__ = ("cache", "max_size", "default_ttl")
-    NOT_FOUND = object()
-
-    class Entry:
-        __slots__ = ("value", "expires")
-
-        def __init__(self, value, ttl):
-            self.value = value
-            logging.debug(f"VALUE: {value} TTL: {ttl}")
-            self.expires = time.time() + ttl if ttl != 0 else None
-
-        @property
-        def is_expired(self):
-            return self.expires is not None and time.time() > self.expires
-
-    def __init__(self, max_size=1024, default_ttl=0):
-        self.cache = collections.OrderedDict()
-        self.max_size = max_size
-        self.default_ttl = default_ttl
-
-    def get(self, key):
-        """Gets the value of a single key in the cache. Move to end of LRU
-        queue. Returns LRUCache.NOT_FOUND if no key is there. O(1) time.
-        """
-        entry = self.cache.get(key)
-        if entry is None:
-            return LRUCache.NOT_FOUND
-        if entry.is_expired:
-            del self.cache[key]
-            return LRUCache.NOT_FOUND
-        self.cache.move_to_end(key)
-        return entry.value
-
-    def set(self, key, value, ttl: float = 0) -> None:
-        """Sets the value of a single key in the cache. Added to end of LRU
-        queue. O(1) time.
-        """
-        ttl = ttl or self.default_ttl
-        self.cache[key] = LRUCache.Entry(value, ttl or self.default_ttl)
-        self.cache.move_to_end(key)
-
-        while len(self.cache) > self.max_size:
-            self.cache.popitem(last=False)
-
-    def clear(self, key) -> None:
-        """Invalidates a single key in the cache."""
-        if key in self.cache:
-            del self.cache[key]
-
-    def flush(self, key) -> None:
-        """Flushes all TTL-expired items from the cache."""
-        pairs = ((key, value) for key, value in self.cache.items()
-                 if not value.is_expired)
-        self.cache = collections.OrderedDict(pairs)
-
-
 class KeyValueStore(ABC):
 
     @abstractmethod
@@ -230,28 +169,17 @@ class Cache(KeyValueStore):
     def __init__(self, store, *,
                  key_coder=IdentityCoder(),
                  value_coder=IdentityCoder(),
-                 local_cache_size=2048,
                  timeout=None):
         self.store = store
         self.key_coder = key_coder
         self.value_coder = value_coder
 
-        # FIXME: This will not work when we need to scale to multiple
-        # processes/machines. Implement proper server-assisted client-side
-        # caching.
-        self.local_cache = LRUCache(max_size=local_cache_size,
-                                    default_ttl=300)
-
     async def get(self, key):
         """|coro| Get the value for a key and field. Atomicity depends on
         underlying store.
         """
-        cached = self.local_cache.get(key)
-        if cached != LRUCache.NOT_FOUND:
-            return cached
         value = await self.store.get(self.key_coder.encode(key))
         ret_val = None if value is None else self.value_coder.decode(value)
-        self.local_cache.set(key, ret_val, ttl=self.store.timeout)
         return ret_val
 
     async def set(self, key, message):
@@ -260,40 +188,25 @@ class Cache(KeyValueStore):
         """
         await self.store.set(self.key_coder.encode(key),
                              self.value_coder.encode(message))
-        self.local_cache.clear(key)
 
     async def clear(self, key):
         """|coro| Deletes the value for a key and field. Atomicity depends on
         underlying store.
         """
         await self.store.clear(self.key_coder.encode(key))
-        self.local_cache.clear(key)
 
     async def get_all(self, keys):
         """|coro| Deletes the value for a key and field. Atomicity depends on
         underlying store.
         """
-        # Check local cache for present keys
-        local_cache = [self.local_cache.get(key) for key in keys]
-        key_count = len(local_cache)
-        local_cache = {key: value for key, value in zip(keys, local_cache)
-                       if value != LRUCache.NOT_FOUND}
-        if len(local_cache) == key_count:
-            # Everything in cache
-            return local_cache
-
         # Fetch missing in remote
-        encoded_keys = [self.key_coder.encode(key) for key in keys
-                        if key not in local_cache]
+        encoded_keys = [self.key_coder.encode(key) for key in keys]
         results = await self.store.get_all(encoded_keys)
-        ret_val = {
+        return {
             self.key_coder.decode(key):
             self.value_coder.decode(value) if value is not None else None
             for key, value in results.items()
         }
-        for key, value in ret_val.items():
-            self.local_cache.set(key, value, ttl=self.store.timeout)
-        return {**local_cache, **ret_val}
 
     async def set_all(self, mapping):
         """|coro| Deletes the value for a key and field. Atomicity depends on
@@ -302,8 +215,6 @@ class Cache(KeyValueStore):
         mapping = {self.key_coder.encode(key): self.value_coder.encode(value)
                    for key, value in mapping.items()}
         await self.store.set_all(mapping)
-        for key in mapping.keys():
-            self.local_cache.clear(key)
 
     def getter(self, func, key_func):
         """Decorator function that caches a getter function for a Protobuffer.
