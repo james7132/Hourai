9bd2cf8936cf9089ac15f7a0a62a2bf3c57dbc91
james7132 <contact@jamessliu.com>
2021-02-17 18:21:41 -0800

    Set up logging for logger.

--- logger/src/hourai/bot/event_handler.rs
+++ logger/src/hourai/bot/event_handler.rs
@@ -1,7 +1,7 @@
 use crate::hourai::db::MemberRoles;
 use twilight_gateway::Event;
 use twilight_model::gateway::payload::*;
-use tracing::error;
+use tracing::{debug, error};
 
 #[derive(Clone)]
 pub struct EventHandler {
@@ -12,9 +12,9 @@ impl EventHandler {
     pub fn consume_event(&self, shard_id: u64, event: twilight_gateway::Event) {
         match event {
             Event::MessageCreate(_) =>
-                println!("Shard {} got a MESSAGE_CREATE event.", shard_id),
+                debug!("Shard {} got a MESSAGE_CREATE event.", shard_id),
             Event::MemberUpdate(evt) => {
-                println!("Shard {} got a GUILD_MEMBER_UPDATE event.", shard_id);
+                debug!("Shard {} got a GUILD_MEMBER_UPDATE event.", shard_id);
                 tokio::spawn(self.clone().member_update(*evt));
             },
             _ => panic!("Unexpected event type: {:?}", event),
--- logger/src/main.rs
+++ logger/src/main.rs
@@ -2,6 +2,7 @@ mod config;
 mod hourai;
 
 use crate::hourai::Hourai;
+use tracing::debug;
 use std::env;
 use std::path::Path;
 use std::path::PathBuf;
@@ -12,7 +13,6 @@ mod proto {
 }
 
 const DEFAULT_ENV: &'static str = "dev";
-const CONFIG_DIR: &'static str = "/etc/hourai";
 
 fn get_config_path() -> Box<Path> {
     let execution_env: String = match env::var("HOURAI_ENV") {
@@ -20,16 +20,23 @@ fn get_config_path() -> Box<Path> {
         Err(_) => String::from(DEFAULT_ENV),
     }.to_lowercase();
 
-    let mut buffer = PathBuf::new();
-    buffer.push(CONFIG_DIR);
-    buffer.set_file_name(execution_env);
-    buffer.set_extension("json");
+    let mut buffer: PathBuf = ["/etc", "hourai"].iter().collect();
+    buffer.push(execution_env);
     return buffer.into_boxed_path();
 }
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_level(true)
+        .with_thread_ids(true)
+        .with_timer(tracing_subscriber::fmt::time::ChronoUtc::rfc3339())
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .compact()
+        .init();
+
     let config = config::load_config(get_config_path().as_ref());
+    debug!("Loaded Config: {:?}", config);
     let mut hourai = Hourai::new(config).await;
     hourai.run().await;
 }
