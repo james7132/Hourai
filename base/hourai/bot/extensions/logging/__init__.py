5dd4f6254b00e6a75bfeec63c4f4cbc45a2b8a34
james7132 <contact@jamessliu.com>
2021-03-19 06:36:23 -0700

    Remove in-memory counters from Python implementation
    Also remove unused "on_raw_member_remove" event.

--- base/hourai/bot/extensions/logging/__init__.py
+++ base/hourai/bot/extensions/logging/__init__.py
@@ -1,13 +1,11 @@
 from .mod_logging import ModLogging
 from .owner_logging import OwnerLogging
 from .role_logging import RoleLogging
-from .counters import Counters
 
 
 def setup(bot):
     cogs = (ModLogging(bot),
             OwnerLogging(bot),
-            RoleLogging(bot),
-            Counters(bot))
+            RoleLogging(bot))
     for cog in cogs:
         bot.add_cog(cog)
