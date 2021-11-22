55a3788e5f17a2dd1c5d605b1a4d1f34469fef87
james7132 <contact@jamessliu.com>
2021-11-21 21:22:53 -0800

    Fix app_data usage in web

--- logger/hourai-web/src/main.rs
+++ logger/hourai-web/src/main.rs
@@ -1,5 +1,4 @@
 mod guild_config;
-mod logger;
 mod oauth;
 mod prelude;
 mod status;
@@ -29,20 +28,21 @@ async fn main() -> std::io::Result<()> {
     let config = config::load_config(config::get_config_path().as_ref());
     init::init(&config);
 
+    let port = config.web.port;
     let sql = hourai_sql::init(&config).await;
     let redis = hourai_redis::init(&config).await;
-    let port = config.web.port;
 
     HttpServer::new(move || {
         App::new()
-            .wrap(logger::TracingLogger)
-            .app_data(AppState {
+            .wrap(tracing_actix_web::TracingLogger::default())
+            .app_data(web::Data::new(AppState {
                 config: config.clone(),
                 http: awc::Client::new(),
                 sql: sql.clone(),
                 redis: redis.clone(),
-            })
-            .service(web::scope("/api").configure(api))
+            }))
+            .service(web::scope("/api")
+            .configure(api))
     })
     .bind(format!("0.0.0.0:{}", port))?
     .run()
